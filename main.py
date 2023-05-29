46dbceb7cd766997daa9d2082f9a760c6c1e4562
Hal <hal@int.pl>
2023-05-29 12:41:44 +0200

    Improve performance using numba in calculating node strengths

--- main.py
+++ main.py
@@ -1,28 +1,32 @@
-from pyparsing import results
-
 from model import Model
 from visualizer import Visualizer
 import cProfile
 import pstats
 
+timeit = False
+
+
+def main():
+    params = {
+        'm0': 4,
+        'm': 2,
+        'w0': 1,
+        'delta': 9,
+        'T': 10,
+    }
+
+    connections = Model(**params).run()
+    vis = Visualizer(connection_matrix=connections)
+    # vis.node_degrees()
+
 if __name__ == '__main__':
     with cProfile.Profile() as profile:
-
-        params = {
-            'm0': 4,
-            'm': 2,
-            'w0': 1,
-            'delta': 9,
-            'T': 6,
-        }
-
-        # connections = Model(**params).run()
-        connections = Model(**params).run()
-        vis = Visualizer(connection_matrix=connections)
-
-        # vis.node_degrees()
+        main()
 
         # -----------------------------------------------------------------
         results = pstats.Stats(profile)
         results.sort_stats(pstats.SortKey.CUMULATIVE)
-        results.print_stats(5)
+
+        if timeit:
+            results.print_stats(5)
+
--- model.py
+++ model.py
@@ -1,6 +1,5 @@
 from typing import Iterable
-from numba import njit
-from numba.experimental import jitclass
+from numba import njit, prange, types
 import numpy as np
 
 
@@ -21,18 +20,37 @@ class Model:
         self.T = T
 
         self.num_of_nodes = 0
-        self.connections = np.zeros(shape=(m0 + T, m0 + T), dtype=float)
+        self.connections = np.zeros(shape=(m0 + T, m0 + T), dtype=np.float64)
 
         self._initialize()
 
+    @staticmethod
+    @njit("f8[:](f8[:,:])", cache=True, fastmath=True, parallel=True)
+    def __normalized_strengths(cons: np.ndarray) -> np.ndarray:
+        N = cons.shape[0]
+        strengths = np.zeros(N, dtype=np.float64)
+        for j in prange(N):
+            for i in range(N):
+                strengths[j] += cons[j][i]
+
+        return strengths / strengths.sum()
+
     def _draw_m_nodes(self) -> list[int]:
-        weights = self.connections[:, :self.num_of_nodes].sum(axis=0)
+        existing_connections = self.connections[
+                               :self.num_of_nodes, :self.num_of_nodes]
+
+        # Numba outperforms numpy if num of nodes > 2000.
+        if self.num_of_nodes < 2000:
+            strengths = existing_connections.sum(axis=0)
+            normalized_strengths = strengths / strengths.sum()
+        else:
+            normalized_strengths = self.__normalized_strengths(existing_connections)
 
         return np.random.choice(
             a=range(self.num_of_nodes),
             size=self.m,
             replace=False,
-            p=weights / weights.sum(),
+            p=normalized_strengths,
         )
 
     def _add_connections(self, new_node: int, existing_nodes: Iterable):
@@ -73,10 +91,11 @@ class Model:
         self.num_of_nodes = self.m0
 
     def show_network(self) -> None:
-        with np.printoptions(precision=1, suppress=True):
-            print(f't = {self.t}')
-            print(self.connections[:self.num_of_nodes, :self.num_of_nodes])
-            print()
+        pass
+        # with np.printoptions(precision=1, suppress=True):
+        #     print(f't = {self.t}')
+        #     print(self.connections[:self.num_of_nodes, :self.num_of_nodes])
+        #     print()
 
     def step(self) -> None:
         self.show_network()
